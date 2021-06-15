10a4a32b90db06809f2562aaac571073ba2a42da
Matias Enrique Pavan <matiaspavan@gmail.com>
2021-06-14 22:21:24 -0300

    added lost data from commit

--- app/src/main/java/ar/edu/itba/fitness/buddy/navigation/routine/RoutinePreviewFragment.java
+++ app/src/main/java/ar/edu/itba/fitness/buddy/navigation/routine/RoutinePreviewFragment.java
@@ -46,7 +46,6 @@ import ar.edu.itba.fitness.buddy.model.ExerciseCard;
 
 public class RoutinePreviewFragment extends Fragment {
 
-    private CycleCard warmupCycle, cooldownCycle;
     private final String name;
     private final ArrayList<CycleCard> workoutCycles = new ArrayList<>();
     private RecyclerView cycleRecycler;
@@ -128,13 +127,21 @@ public class RoutinePreviewFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_routine_preview, container, false);
-        View warmupView = view.findViewById(R.id.warmup_cycle);
-        View cooldownView = view.findViewById(R.id.cooldown_cycle);
 
         FloatingActionButton routineExecutor = view.findViewById(R.id.routine_executor);
-        cycleRecycler = view.findViewById(R.id.cycle_recycler);
-        cycleRecycler.setHasFixedSize(true);
-        cycleRecycler.setLayoutManager(new LinearLayoutManager(view.getContext()));
+        for(int i=0;i<3;i++){
+            switch (i){
+                case 0: cycleRecycler = view.findViewById(R.id.warmup_cycle_recycler);
+                    break;
+                case 1: cycleRecycler = view.findViewById(R.id.exercise_cycle_recycler);
+                    break;
+                case 2: cycleRecycler = view.findViewById(R.id.cooldown_cycle_recycler);
+                    break;
+            }
+            cycleRecycler.setHasFixedSize(true);
+            cycleRecycler.setLayoutManager(new LinearLayoutManager(view.getContext()));
+            getWorkoutCycles(i);
+        }
 
         routineExecutor.setOnClickListener((l) -> {
             FragmentTransaction transaction = requireActivity().getSupportFragmentManager().beginTransaction().setReorderingAllowed(true);
@@ -142,7 +149,7 @@ public class RoutinePreviewFragment extends Fragment {
             transaction.commit();
         });
 
-        getWorkoutCycles();
+
 
         return view;
     }
@@ -178,47 +185,50 @@ public class RoutinePreviewFragment extends Fragment {
             }
         });
     }*/
-    void getWorkoutCycles(){
-        App app = (App)requireActivity().getApplication();
-        app.getRoutineRepository().getRoutineCycles(id,0,10,"id","asc").observe(getViewLifecycleOwner(), e -> {
-            if (e.getStatus() == Status.SUCCESS) {
-                PagedList<Cycle> cyclePage = e.getData();
-                if(cyclePage != null) {
-                    ArrayList<Cycle> cycles= (ArrayList<Cycle>)cyclePage.getContent();
-                    ArrayList<CycleCard> workoutCycles=new ArrayList<>();
-                    cycles.forEach(cycle -> {
-                        app.getCycleRepository().getCycleExercises(cycle.getId(),0,10,"exerciseId","asc").observe(getViewLifecycleOwner(),f->{
-                            if(f.getStatus()==Status.SUCCESS){
-                                PagedList<Exercise> exercisePagedList=f.getData();
-                                if(exercisePagedList!=null){
-                                    ArrayList<Exercise> exercises=(ArrayList<Exercise>) exercisePagedList.getContent();
-                                    ArrayList<ExerciseCard> exerciseCards=new ArrayList<>();
-                                    exercises.forEach(exercise -> {
-                                        ExerciseCard exerciseCard=new ExerciseCard(exercise.getExercise().getName(),
-                                                exercise.getDuration(),exercise.getRepetitions());
-                                        exerciseCards.add(exerciseCard);
-                                    });
-                                    if(cycle.getType().compareTo("warmup")==0){
-                                        warmupCycle=new CycleCard(cycle.getId(),cycle.getName(),cycle.getRepetitions(),exerciseCards);
-                                    }else if(cycle.getType().compareTo("cooldown")==0){
-                                        cooldownCycle=new CycleCard(cycle.getId(),cycle.getName(),cycle.getRepetitions(),exerciseCards);
-                                    }else{
-                                        CycleCard cycleCard=new CycleCard(cycle.getId(),cycle.getName(),cycle.getRepetitions(),exerciseCards);
-                                        workoutCycles.add(cycleCard);
-                                    }
-                                }
-                            }else{
-                                defaultResourceHandler(f);
-                            }
-                        });
-                    });
-                    adapter = new CycleCardAdapter(workoutCycles);
-                    cycleRecycler.setAdapter(adapter);
-                }
-            }else {
-                defaultResourceHandler(e);
-            }
-        });
-    }
+   void getWorkoutCycles(int recyclerIndex){
+       App app = (App)requireActivity().getApplication();
+       app.getRoutineRepository().getRoutineCycles(id,0,10,"id","asc").observe(getViewLifecycleOwner(), e -> {
+           if (e.getStatus() == Status.SUCCESS) {
+               PagedList<Cycle> cyclePage = e.getData();
+               if(cyclePage != null) {
+                   ArrayList<Cycle> cycles= (ArrayList<Cycle>)cyclePage.getContent();
+                   ArrayList<CycleCard> workoutCycles=new ArrayList<>();
+                   cycles.forEach(cycle -> {
+                       app.getCycleRepository().getCycleExercises(cycle.getId(),0,10,"exerciseId","asc").observe(getViewLifecycleOwner(),f->{
+                           if(f.getStatus()==Status.SUCCESS){
+                               PagedList<Exercise> exercisePagedList=f.getData();
+                               if(exercisePagedList!=null){
+                                   ArrayList<Exercise> exercises=(ArrayList<Exercise>) exercisePagedList.getContent();
+                                   ArrayList<ExerciseCard> exerciseCards=new ArrayList<>();
+                                   exercises.forEach(exercise -> {
+                                       ExerciseCard exerciseCard=new ExerciseCard(exercise.getExercise().getName(),
+                                               exercise.getDuration(),exercise.getRepetitions());
+                                       exerciseCards.add(exerciseCard);
+                                   });
+                                   switch (recyclerIndex){
+                                       case 0: if(cycle.getType().compareTo("warmup")==0) {
+                                           workoutCycles.add(new CycleCard(cycle.getId(),cycle.getName(),cycle.getRepetitions(),exerciseCards));
+                                       }break;
+                                       case 1: workoutCycles.add(new CycleCard(cycle.getId(),cycle.getName(),cycle.getRepetitions(),exerciseCards));
+                                           break;
+                                       case 2:  if(cycle.getType().compareTo("cooldown")==0) {
+                                           workoutCycles.add(new CycleCard(cycle.getId(),cycle.getName(),cycle.getRepetitions(),exerciseCards));
+                                       }break;
+                                   }
+
+                               }
+                           }else{
+                               defaultResourceHandler(f);
+                           }
+                       });
+                   });
+                   adapter = new CycleCardAdapter(workoutCycles);
+                   cycleRecycler.setAdapter(adapter);
+               }
+           }else {
+               defaultResourceHandler(e);
+           }
+       });
+   }
 
 }
